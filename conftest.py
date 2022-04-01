82e3b89261a95e1e5ec5715caafc073f79abcaba
igorabdreev <igor.abdreev@gmail.com>
2022-04-01 11:19:46 +0300

    Изменения

--- conftest.py
+++ conftest.py
@@ -1,16 +1,16 @@
-from selenium import webdriver
 import pytest
+from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
+from selenium.webdriver.common.by import By
 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default="en",
-                     help="Choose language: ar ca cs da de el es fi fr it ko nl pl pt pt-br ro ru sk uk zh-hans en-gb")
+                     help="Choose language: ar ca cs da de el es fi fr it ko nl pl pt pt-br ro ru sk uk zh-hans en")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
-    languages = "ar ca cs da de el es fi fr it ko nl pl pt pt-br ro ru sk uk zh-hans en-gb "
+    languages = "ar ca cs da de el es fi fr it ko nl pl pt pt-br ro ru sk uk zh-hans en "
     language = request.config.getoption("language")
     if (language + " ") in languages:
         print("\nstart chrome browser for test..")
@@ -19,7 +19,7 @@ def browser(request):
             'prefs', {'intl.accept_languages': language})
         browser = webdriver.Chrome(options=options)
     else:
-        print("\nlanguage {} not supported :(\ntry: ar ca cs da de el es fi fr it ko nl pl pt pt-br ro ru sk uk zh-hans en-gb".format(language))
+        print("\nlanguage {} not supported :(\ntry: ar ca cs da de el es fi fr it ko nl pl pt pt-br ro ru sk uk zh-hans en".format(language))
         pytest.fail("Wrong Language")
         # assert 0
     yield browser
