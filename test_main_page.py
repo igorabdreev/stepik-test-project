ea3ba4b83b5629d9ce0e2450fc28bdefd1c8f159
igorabdreev <igor.abdreev@gmail.com>
2022-04-01 13:22:39 +0300

    комментарии в код

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,13 @@
 from selenium.webdriver.common.by import By
 # py -m pytest -v --tb=line --language=en test_main_page.py
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+link = "http://selenium1py.pythonanywhere.com/"
+
+def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
+
+def test_guest_can_go_to_login_page(browser): # путест вызывает только эту функцию, т.к. она
+   browser.get(link)                          # она начинается на TEST...
+   go_to_login_page(browser)
 
