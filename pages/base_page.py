95a970d394e6e89a6041f631d0b52955c431fb14
igorabdreev <igor.abdreev@gmail.com>
2022-04-02 20:35:35 +0300

    Проверка исключения

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,18 @@
-
-url =   "http://selenium1py.pythonanywhere.com/catalogue/category/books_2/"
+from selenium.common.exceptions import NoSuchElementException
+#url =   "http://selenium1py.pythonanywhere.com/catalogue/category/books_2/"
 
 class BasePage():            # вспомогательные методы для работы с драйвером
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 # py -m pytest -v --tb=line --language=en test_main_page.py
+from pages.main_page import MainPage
 
 link = "http://selenium1py.pythonanywhere.com/"
 
@@ -7,7 +8,12 @@ def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
 
-def test_guest_can_go_to_login_page(browser): # путест вызывает только эту функцию, т.к. она
-   browser.get(link)                          # она начинается на TEST...
-   go_to_login_page(browser)
+ # путест вызывает только эту функцию, т.к. она она начинается на TEST...
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
+    #page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+    page.should_be_login_link()
 
