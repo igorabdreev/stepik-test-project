79bf17625b26c1039b8451ba89b09e8c0aea8772
igorabdreev <igor.abdreev@gmail.com>
2022-04-03 18:52:14 +0300

    Храним Селекторы

---
+++ pages/locators.py
@@ -0,0 +1,14 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    LOGIN_FORM_EMAIL = (By.CSS_SELECTOR, "#id_login-username")
+    LOGIN_FORM_PASSWORD = (By.CSS_SELECTOR, "#id_login-password")
+
+    REGISER_FORM_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISER_FORM_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISER_FORM_PASSWORD2 = (By.CSS_SELECTOR, "#id_registration-password2")
+
