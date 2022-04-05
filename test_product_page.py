a0808c37343e5420ebc23f1aa730e4d2fd3204c1
igorabdreev <igor.abdreev@gmail.com>
2022-04-05 16:39:28 +0300

    Проверка множество сайтов

--- test_product_page.py
+++ test_product_page.py
@@ -2,13 +2,27 @@ from selenium.webdriver.common.by import By
 # py -m pytest -v --tb=line --language=en test_main_page.py
 from pages.main_page import MainPage
 from pages.product_page import ProductPage
+import pytest
 import time
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser,link): #добавил линк
+    #link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    #page.get(link)
     page.open()  # открываем страницу
     page.add_to_cart()
     page.solve_quiz_and_get_code()
     page.should_be_message_about_adding()
     #time.sleep(11000)
     #py -m pytest -s test_product_page.py
+
+    #browser.get(language)
